06742fa397c657aa40350b5f27f41d193480d99b
Eli <eli.k.miller@gmail.com>
2020-04-17 16:17:00 -0500

    replace clips

--- src/AudioContext.js
+++ src/AudioContext.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import axios from "axios";
 
 const _audioContext = new AudioContext();
@@ -6,29 +6,91 @@ const ReactAudioContext = React.createContext();
 
 let audioData = {};
 const clips = [
-  "clap",
-  "clave",
-  "conga",
-  "cowbell",
-  "hihatopen",
-  "maracas",
-  "ride",
-  "rim",
-  "stick",
-  "tom",
-  "snare",
-  "kick",
+  "china",
+  "crash_a",
+  "crash_b",
+  "crash_c",
   "hihat",
+  "hihat_open",
+  "kick_a",
+  "kick_b",
+  "kick_ghost",
+  "ride",
+  "snare_a",
+  "snare_b",
+  "splash",
+  "tom_high",
+  "tom_low",
+  "tom_mid",
 ];
 
-const playSound = (clip) => {
+const playSound = (clip, start = 0) => {
+  let currentTime = _audioContext.currentTime;
   let source = _audioContext.createBufferSource();
   source.buffer = audioData[clip];
   source.connect(_audioContext.destination);
-  source.start(0);
+  if (start === 0) source.start(0);
+  else source.start(currentTime + start);
 };
 
 const AudioContextProvider = ({ children }) => {
+  const [tempo, setTempo] = useState(90);
+  const sequence = {
+    hihat: [
+      true,
+      false,
+      true,
+      false,
+      true,
+      false,
+      true,
+      false,
+      true,
+      false,
+      true,
+      false,
+      true,
+      false,
+      true,
+      false,
+    ],
+    snare_a: [
+      false,
+      false,
+      false,
+      false,
+      true,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      true,
+      false,
+      false,
+      false,
+    ],
+    kick_a: [
+      true,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      true,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+    ],
+  };
   useEffect(() => {
     for (let clip of clips) {
       axios
@@ -44,8 +106,20 @@ const AudioContextProvider = ({ children }) => {
     }
   }, []);
 
+  const playSequence = () => {
+    for (let i = 0; i < 16; i++) {
+      for (let clip of Object.keys(sequence)) {
+        if (sequence[clip][i]) {
+          playSound(clip, (60 / (tempo * 4)) * i);
+        }
+      }
+    }
+  };
+
   return (
-    <ReactAudioContext.Provider value={{ _audioContext, clips, playSound }}>
+    <ReactAudioContext.Provider
+      value={{ _audioContext, tempo, setTempo, clips, playSound, playSequence }}
+    >
       {children}
     </ReactAudioContext.Provider>
   );
--- src/DrumMachine.js
+++ src/DrumMachine.js
@@ -3,13 +3,18 @@ import { useAudioContext } from "./AudioContext";
 
 const Pad = ({ clip }) => {
   const audioContext = useAudioContext();
+  if (!clip)
+    return (
+      <button className="Pad" type="button">
+        <em>Unassigned</em>
+      </button>
+    );
   return (
     <button
       className="Pad"
-      onClick={() => {
+      onMouseDown={() => {
         audioContext.playSound(clip);
       }}
-      key={clip}
       type="button"
     >
       {clip}
@@ -21,10 +26,18 @@ const DrumMachine = () => {
   const audioContext = useAudioContext();
 
   return (
-    <div>
-      {audioContext.clips.map((clip) => (
-        <Pad clip={clip} key={clip} />
-      ))}
+    <div className="DrumMachine">
+      <div className="PadController">
+        <div className="DrumMachine-logo">
+          <strong>DRUM</strong>MACHINE
+        </div>
+        {audioContext.clips.map((clip, i) => (
+          <React.Fragment key={i}>
+            <Pad clip={clip} />
+            {(i + 1) % 4 === 0 && <br />}
+          </React.Fragment>
+        ))}
+      </div>
     </div>
   );
 };
