d74e2c39ac3565d661e61b9185b5f913e60ed049
Eli K Miller <eli@elikmiller.com>
2021-01-31 22:36:30 -0800

    Style tweaks :)

--- src/components/TrackArray.js
+++ src/components/TrackArray.js
@@ -3,7 +3,17 @@ import NoteToggleButton from "./NoteToggleButton";
 
 const TrackArray = ({ trackName, trackNotes, lastNotePlayed, isPlaying, toggleNote }) => {
   return (
-    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", marginTop: "20px" }}>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: "10px",
+        padding: "5px",
+        border: "1px solid rgba(255, 255, 255, 0.75)",
+        borderRadius: "4px",
+      }}
+    >
       <div style={{ minWidth: "96px" }}>{trackName}</div>
       {trackNotes.map((note, i) => (
         <NoteToggleButton key={i} isActive={note} isPlaying={(i + 1) % 16 === lastNotePlayed} onClick={() => toggleNote(trackName, i)} />
