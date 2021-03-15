da861db2dd51ea4be3ddb8327e25b8eeb656186e
Eli K Miller <eli@elikmiller.com>
2021-03-15 10:07:25 -0700

    Allow initial pattern and tempo to be configured in DrumMachine

--- src/DrumMachine.js
+++ src/DrumMachine.js
@@ -8,6 +8,25 @@ class DrumMachine {
       onNotePlayed: () => {},
       onPatternChange: () => {},
       onLoadingDone: () => {},
+      pattern: {
+        clap: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        clave: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        conga_hi: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        conga_mid: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        conga_low: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        cowbell: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        cymbal: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        hihat_closed: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        hihat_open: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        kick: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        maracas: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        rim: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        snare: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        tom_hi: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        tom_mid: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        tom_low: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+      },
+      tempo: 80.0
     }
   ) {
     this.audioContext = new AudioContext();
@@ -15,30 +34,13 @@ class DrumMachine {
     this.isPlaying = false; // Are we currently playing?
     this.startTime = null; // The start time of the entire sequence.
     this.current16thNote = null; // What note is currently last scheduled?
-    this.tempo = 80.0; // tempo (bpm)
+    this.tempo = options.tempo; // tempo (bpm)
     this.lookahead = 25.0; // How frequently to call scheduling function (ms)
     this.scheduleAheadTime = 0.1; // How far ahead to schedule audio (s)
     this.nextNoteTime = 0.0; // when the next note is due.
     this.noteLength = 0.05; // length of "beep" (in seconds)
     this.timerID = null; // setInterval id
-    this.pattern = {
-      clap: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-      clave: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-      conga_hi: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-      conga_mid: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-      conga_low: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-      cowbell: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-      cymbal: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-      hihat_closed: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-      hihat_open: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-      kick: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-      maracas: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-      rim: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-      snare: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-      tom_hi: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-      tom_mid: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-      tom_low: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
-    };
+    this.pattern = options.pattern;
 
     // callbacks
     this.onTempoChange = options.onTempoChange;
